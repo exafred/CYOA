03605dd1fd75142c3c1974688a30f0ff5282acbd
exafred <hyoung8778@gmail.com>
2015-05-15 15:20:16 +0100

    Management Side Upgrades
    Adding structure and CSS skeleton for management side of things

--- inc/autoselect.php
+++ inc/autoselect.php
@@ -2,66 +2,10 @@
 	if(isset($_POST)) {
 		
 		include('mysqli_connect.php');
-				
-		function getList($post, $conn) {
-			switch ($post['type']) {
-				// get list of books
-				case "book":
-					$query = "SELECT
-					id as 'book_id',
-					title,
-					subtitle
-					FROM `book`;";
-				break;
-				case "page":
-				// get list of pages within a given book
-					if($post['book_id'] != "false") {
-						$book_id = mysql_real_escape_string($post['book_id'], $dbc);
-						$query = "
-						SELECT 
-							book_id,
-							number as 'page_number',
-							title,
-							content
-						FROM `page`
-						WHERE `book_id` = $book_id
-						ORDER BY `number` ASC;";
-					} else {
-						$query = false;
-					}
-				break;
-				case "choice":
-				// get list of choices for a given book's given page
-					if($post['book_id'] != "false" && $post['page_number'] != "false") {
-						$book_id = mysql_real_escape_string($post['book_id'], $dbc);
-						$page_number = mysql_real_escape_string($post['page_number'], $dbc);
-						$query = "
-						SELECT
-							*
-						FROM `choice`
-						WHERE `book_id` = '$book_id'
-						AND `page_number` = '$page_number'
-						ORDER BY `number` ASC;";
-					} else {
-						$query = false;
-					}
-				break;
-				default: 
-					$query = false;
-				break;
-			}
+		
+		function getInfo($data, $conn) {
 			
-			if($query != false) {
-				$queryresult = mysqli_query($conn, $query);
-				$result = array("result"=>array());
-				while($r = mysqli_fetch_assoc($queryresult)) {
-					$result["result"][] = $r;
-				}
-				return $result;
-			} else {
-				return "Failure";
-			}
 		}
-		echo json_encode(getList($_POST, $dbc));
+		echo json_encode(getInfo($_POST, $dbc));
 	}
 ?>
--- manage/index.php
+++ manage/index.php
@@ -3,37 +3,199 @@
 	include('../inc/mysqli_connect.php');
 ?>
 <?php
-	$booksquery = "SELECT * FROM `book`;";
-	$booksresult = mysqli_query($dbc, $booksquery);
-	
-	$books = array();
-	while($b = mysqli_fetch_assoc($booksresult)) {
-		$books[] = $b;
+
+	if(isset($_GET['b']) && is_numeric($_GET['b']) && $_GET['b'] > 0) {
+		// we have a book; get book details
+		$current_book = $_GET['b'];
+		
+		#///////
+		$book_query = "SELECT * FROM `book` WHERE `id` = ".$current_book.";";
+		$book_result = mysqli_query($dbc, $book_query);
+		
+		// if only a single book is returned
+		if(mysqli_num_rows($book_result) == 1) {
+			// list book
+			$book = mysqli_fetch_assoc($book_result);
+			
+			// echo out book details
+			
+			echo 'book details';
+			ppa($book);
+			makeSingleBook($book);
+			
+			// do we also have a page from this book?
+			if(isset($_GET['p']) && is_numeric($_GET['p']) && $_GET['p'] > 0) {
+				// get current page details, too
+				$current_page = $_GET['p'];
+				
+				#///////
+				$page_query = "
+				SELECT 
+					*
+				FROM `page`
+				WHERE `book_id` = ".$current_book."
+				AND `id` = ".$current_page."
+				ORDER BY `number` ASC
+				;";
+				$page_result = mysqli_query($dbc, $page_query);
+				
+				if(mysqli_num_rows($page_result) == 1) {
+					$page = mysqli_fetch_assoc($page_result);
+					
+					// also get page's existing choices
+								
+					#///////
+					$choicesquery = "SELECT
+						*
+					FROM `choice`
+					WHERE `book_id` = ".$current_book."
+					AND `page_number` = ".$current_page."
+					ORDER BY `sort_order`
+					;";
+					$choicesresult = mysqli_query($dbc, $choicesquery);
+					
+					if(mysqli_num_rows($choicesresult) > 0) {
+						$page['choices'] = array();
+						while($c = mysqli_fetch_assoc($choicesresult)) {
+							$page['choices'][] = $c;
+						}
+					}
+					// echo page details
+					echo 'page details, with ('.count($page).') choices';
+					ppa($page);
+					makeSinglePage($page);
+				}
+			} else {
+				// no set page, get a list of all pages from this book
+				#///////
+				$pagesquery = "
+				SELECT 
+					`page`.*,
+					COUNT(`choice`.`id`) as num_choices
+				FROM `page`
+				LEFT JOIN `choice`
+				ON `choice`.`page_number` = `page`.`id`
+				WHERE `page`.`book_id` = ".$current_book."
+				ORDER BY `number` ASC;";
+				$pagesresult = mysqli_query($dbc, $pagesquery);
+				
+				if(mysqli_num_rows($pagesresult) > 0) {
+					$pages = array();
+					while($p = mysqli_fetch_assoc($pagesresult)) {
+						$pages[$p['number']] = $p;
+						ok somehow the stuff isn't coming through here for all pages' - do something... Fixy?
+					}
+					
+					// loop through all pages & echo contents
+					echo 'page list w/ part-content of '.count($pages).' pages';
+					ppa($pages);
+					makePagesList($pages);
+				}
+			}
+			echo '<div class="make new page fa fa-plu">New Page</div>';
+		} else {
+			// no existing book! display message and offer backlink
+			echo 'no book; provide backlink to main management page';
+		}
+	} else {
+		// we don't have a book chosen; get a list of all books
+		#///////
+		$booksquery = "SELECT
+			`book`.*,
+			COUNT(`page`.`id`) AS num_pages
+		FROM `book`
+		LEFT JOIN `page`
+		ON `page`.`book_id` = `book`.`id`
+		;";
+		$booksresult = mysqli_query($dbc, $booksquery);
+		
+		if(mysqli_num_rows($booksresult) > 0) {
+			$books = array();
+			while($b = mysqli_fetch_assoc($booksresult)) {
+				$books[] = $b;
+			}
+			
+			// list all books
+			echo 'books list!';
+			ppa($books);
+			makeBooksList($books);
+		}
+		echo '<div class="make new book fa fa-plu">New Book</div>';
 	}
-	
-	#///////
-	$pagesquery = "
-	SELECT 
-		*
-	FROM `page`
-	ORDER BY `number` ASC;";
-	$pagesresult = mysqli_query($dbc, $pagesquery);
-	
-	$pages = array();
-	while($p = mysqli_fetch_assoc($pagesresult)) {
-		$pages[$p['number']] = $p;
-		$pages[$p['number']]['choices'] = array();
+	function ppa($a) {
+		echo "<PRE>",print_r($a),"</PRE>";
 	}
-	
-	#///////
-	$choicesquery = "SELECT * FROM `choice`;";
-	$choicesresult = mysqli_query($dbc, $choicesquery);
-	
-	while($c = mysqli_fetch_assoc($choicesresult)) {
-		$pages[$c['page_number']]['choices'][] = $c;
+	function makeBooksList($list) {
+		if(is_array($list) && count($list) > 0) {
+			echo '<div class="booklist">';
+			foreach($list as $item) {
+				echo
+				'<div class="item">
+					<div class="title">'.$item['title'].'</div>
+					<div class="subtitle">'.$item['subtitle'].'</div>
+					<div class="number_of_pages">Pages: <span class="number">'.$item['num_pages'].'</span></div>
+					<div class="acknowledgements">'.$item['acknowledgements'].'</div>
+					<div class="notes">'.$item['notes'].'</div>
+					<a class="outref" href="?b='.$item['id'].'"></a>
+				</div>';
+			}
+			echo '</div>';
+		}
 	}
-	
-	#echo "<PRE>",print_r($pages),"</PRE>";
+	function makeSingleBook($item) {
+		echo
+		'<div class="single book">
+			<div class="title">'.$item['title'].'</div>
+			<div class="subtitle">'.$item['subtitle'].'</div>
+			<div class="number_of_pages">Pages: <span class="number">'.$item['num_pages'].'</span></div>
+			<div class="acknowledgements">'.$item['acknowledgements'].'</div>
+			<div class="notes">'.$item['notes'].'</div>
+		</div>';
+	}
+	function makePagesList($list) {
+		if(is_array($list) && count($list) > 0) {
+			echo '<div class="pagelist">';
+			foreach($list as $item) {
+				$visible_class = ($item['publish'] == 1)? ' yes': '';
+				echo
+				'<div class="item">
+					<div class="page_number">Page #<span class="number">'.$item['number'].'</span></div>
+					<div class="title">'.$item['title'].'</div>
+					<div class="content">'.substr($item['content'], 0, 150).'&hellip;</div>
+					<div class="date first_added">'.$item['added'].'</div>
+					<div class="date last_modified">'.$item['last_modified'].'</div>
+					<div class="currently visible'.$visible_class.'"></div>
+					<a class="outref" href="?b='.$item['book_id'].'&p='.$item['id'].'"></a>
+				</div>';
+			}
+			echo '</div>';
+		}
+	}
+	function makeSinglePage($item) {
+		$visible_class = ($item['publish'] == 1)? ' yes': '';
+		echo '<div class="single page" data-id="'.$item['id'].'">
+			<div class="page_number">Page #<span class="number" contenteditable="true">'.$item['number'].'</span></div>
+			<div class="title">'.$item['title'].'</div>
+			<div class="content" contenteditable="true">'.$item['content'].'</div>
+			<div class="currently visible'.$visible_class.'"></div>'. makeChoicesList($item['choices']).'</div>';
+	}
+	function makeChoicesList($list) {
+		if(is_array($list) && count($list) > 0) {
+			echo '<div class="page choices">';
+			foreach($list as $choice) {
+				echo '<div class="choice" data-id="'.$choice['id'].'">
+					<div class="relation">
+						<div class="current target page" data-target="'.$choice['target_page'].'">Target Page: '.$choice['target_page'].'</div>
+						<span class="move_choice">Assign Choice to Different Page</span>
+						<span class="move_target">Change Target Page</span>
+					</div>
+					<div class="choice_text" contenteditable="true">'.$choice['text'].'</div>
+				</div>';
+			}
+			echo '</div>';
+		}
+		echo '<div class="make new choice fa fa-plus"> New Choice</div>';
+	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -41,7 +203,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Manage Your Own Adventure!</title>
 
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
 <link rel="stylesheet" href="../css/main.css" />
 <link rel="stylesheet" href="../css/manage_pages.css" />
 
